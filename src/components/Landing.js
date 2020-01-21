056cdd491dacb5e66838215a3f8dd8d456fbcbf1
Austin Moore <37962463+Austinmoore1492@users.noreply.github.com>
2020-01-21 12:16:33 -0600

    Fixed mobile styles
    Mobile styles weren't optimized, also made the Learn More link stand out more

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Landing = () => (
   <section className="landing">
@@ -8,10 +8,12 @@ const Landing = () => (
         <h1 className="x-large">FLW Connected</h1>
         <p className="lead">A place for Fort Leonard Wood to Connect.</p>
         <p>Coming In 2020</p>
-        <AnchorLink href='#scroll-here' className="scroll-handler">Learn More!</AnchorLink>
+        <AnchorLink href="#scroll-here" className="scroll-handler">
+          Learn More!
+        </AnchorLink>
       </div>
-      
-    <span id="scroll-here"></span>
+
+      <span id="scroll-here" />
     </div>
   </section>
 );
