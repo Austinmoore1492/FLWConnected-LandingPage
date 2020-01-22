864078495815d29b54ff0d423832b0b4b947b155
Austinmoore1492 <austinmoore1492@yahoo.com>
2020-01-21 22:20:03 -0600

    UI improvements for Navbar

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,20 +2,20 @@ import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Landing = () => (
-  <section className="landing">
-    <div className="dark-overlay">
-      <div className="landing-inner">
-        <h1 className="x-large">FLW Connected</h1>
-        <p className="lead">A place for Fort Leonard Wood to Connect.</p>
-        <p>Coming In 2020</p>
-        <AnchorLink href="#scroll-here" className="scroll-handler">
-          Learn More!
-        </AnchorLink>
-      </div>
+	<section id='home' className='landing'>
+		<div className='dark-overlay'>
+			<div className='landing-inner'>
+				<h1 className='x-large'>FLW Connected</h1>
+				<p className='lead'>A place for Fort Leonard Wood to Connect.</p>
+				<p>Coming In 2020</p>
+				<AnchorLink href='#scroll-here' className='scroll-handler'>
+					Learn More!
+				</AnchorLink>
+			</div>
 
-      <span id="scroll-here" />
-    </div>
-  </section>
+			<span id='scroll-here' />
+		</div>
+	</section>
 );
 
 export default Landing;
