3c4e4563f53db44ccf5eed19d5e9966afebb8908
Austin Moore <37962463+Austinmoore1492@users.noreply.github.com>
2020-01-24 07:12:15 -0600

    Adding GOOGLE Analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from "react";
-import "./styles.css";
+import React, { Fragment } from 'react';
+import './styles.css';
 
-import Landing from "./components/Landing";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import SignUp from "./components/SignUp";
+import Landing from './components/Landing';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import SignUp from './components/SignUp';
 
 const App = () => (
   <Fragment>
